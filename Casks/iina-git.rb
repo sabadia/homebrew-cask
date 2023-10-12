0b07220fa8a177bec5fad8f95c21c12c47818ccd
Mahamudul Hasan <mahamudul.hasan@selise.ch>
2023-10-12 15:50:02 +0600

    sha256 updated

--- Casks/iina-git.rb
+++ Casks/iina-git.rb
@@ -1,7 +1,7 @@
 cask "iina-git" do
   version "1.3.3-1"
   arch arm: "arm64", intel: "universal"
-  sha256 "49721d4f700b1267da455306cc8bebc7fe02be11fc8ee49bd8f636ac23783e51"
+  sha256 arm: "49721d4f700b1267da455306cc8bebc7fe02be11fc8ee49bd8f636ac23783e51", intel: "49721d4f700b1267da455306cc8bebc7fe02be11fc8ee49bd8f636ac23783e51"
   url "https://github.com/sabadia/iina/releases/download/#{version}/iina-#{arch}.tar.xz"
   name "iina-git"
   desc "Free and open-source media player"
